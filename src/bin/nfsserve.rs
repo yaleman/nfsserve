e1ca46189293312af36e54ca48963edc763cb377
James Hodgkinson <james@terminaloutcomes.com>
2023-10-02 11:15:01 +1000

    adding CLI config options

--- src/bin/nfsserve.rs
+++ src/bin/nfsserve.rs
@@ -1,14 +1,48 @@
+use clap::Parser;
 use nfsserve::demofs::DemoFS;
 use nfsserve::tcp::*;
 
-const HOSTPORT: u32 = 11111;
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Config {
+    #[arg(short, long)]
+    port: Option<u32>,
+
+    #[arg(short, long)]
+    ip_address: Option<String>,
+}
+
+impl Default for Config {
+    fn default() -> Self {
+        Self {
+            port: Some(11111),
+            ip_address: Some("127.0.0.1".to_string()),
+        }
+    }
+}
+
+impl Config {
+    fn listener_address(&self) -> String {
+        // either they specify one, or they get the default!
+        let listener_port = &self.port.unwrap_or(Config::default().port.unwrap());
+        let listener_ip = self
+            .ip_address
+            .clone()
+            .unwrap_or(Config::default().ip_address.unwrap());
+        format!("{}:{}", listener_ip, listener_port)
+    }
+}
+
 #[tokio::main]
 async fn main() {
     tracing_subscriber::fmt()
         .with_max_level(tracing::Level::DEBUG)
         .with_writer(std::io::stderr)
         .init();
-    let listener = NFSTcpListener::bind(&format!("127.0.0.1:{HOSTPORT}"), DemoFS::default())
+
+    let config = Config::parse();
+
+    let listener = NFSTcpListener::bind(&config.listener_address(), DemoFS::default())
         .await
         .unwrap();
     listener.handle_forever().await.unwrap();
